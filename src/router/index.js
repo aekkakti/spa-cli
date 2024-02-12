098d3f881e2695efac3859cc5ba8201073df7875
aekkakti <music.tomsk1@gmail.com>
2024-02-12 20:25:10 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/');
+}
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/login')
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: function () {
+      return import('../views/HomeView.vue')
+    },
+    beforeEnter: ifAuthenticated,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: function () {
+      return import ('../components/Login.vue')
+    },
+    beforeEnter: ifNotAuthenticated,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
